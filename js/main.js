6249efe28e42ed699134691cfffd63046eb2b82a
saru <tools.customary833@passfwd.com>
2026-01-14 13:44:41 -0500

    sections added

--- js/main.js
+++ js/main.js
@@ -6,10 +6,10 @@ navToggle.addEventListener("click", () => {
 	const isOpen = navMenu.classList.toggle("show-menu");
 
 	if (isOpen) {
-		toggleIcon.src = "../assets/icons/x.svg";
+		toggleIcon.src = "./assets/icons/x.svg";
 		document.body.style.overflow = "hidden";
 	} else {
-		toggleIcon.src = "../assets/icons/menu.svg";
+		toggleIcon.src = "./assets/icons/menu.svg";
 		document.body.style.overflow = "auto";
 	}
 });
@@ -17,7 +17,25 @@ navToggle.addEventListener("click", () => {
 document.querySelectorAll(".overlay-item").forEach((link) => {
 	link.addEventListener("click", () => {
 		navMenu.classList.remove("show-menu");
-		toggleIcon.src = "../assets/icons/menu.svg";
+		toggleIcon.src = "./assets/icons/menu.svg";
 		document.body.style.overflow = "auto";
+	});
+});
+
+// Carousels
+const carouselButtons = document.querySelectorAll(".ctrl-btn");
+
+carouselButtons.forEach((button) => {
+	button.addEventListener("click", () => {
+		const carouselId = button.getAttribute("data-carousel");
+		const direction = parseInt(button.getAttribute("data-dir"));
+		const carousel = document.getElementById(carouselId);
+
+		if (carousel) {
+			carousel.scrollBy({
+				left: 300 * direction,
+				behavior: "smooth",
+			});
+		}
 	});
 });
