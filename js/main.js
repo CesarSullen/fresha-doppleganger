d7a7152b4e331d443e58439ce2b4593c296c313f
saru <tools.customary833@passfwd.com>
2026-01-15 12:47:29 -0500

    sections added

--- js/main.js
+++ js/main.js
@@ -36,3 +36,12 @@ carouselButtons.forEach((button) => {
 		}
 	});
 });
+
+// Accordion
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach((item) => {
+	item.addEventListener("click", () => {
+		item.classList.toggle("active");
+	});
+});
